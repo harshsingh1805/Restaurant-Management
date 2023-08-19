2e25f290e11d0887c87bf3eee92c021abc9bca9d
harshsingh1805 <redminote61805@gmail.com>
2023-08-20 00:42:21 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,39 @@
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click' , () => {
-            console.log('Added To Cart');
+            const productName = button.dataset.productName;
+            
+            let matchingItem;
+            cart.forEach((item) => {
+                if(productName === item.productName){
+                    matchingItem = item;
+ 
+
+                }
+
+            });
+
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }
+
+            else{
+                cart.push({
+                    productName:productName,
+                    quantity:1
+    
+                });
+
+            }
+
+            let cartQunatity = 0;
+
+            cart.forEach((item) => {
+                cartQunatity += item.quantity;
+            });
+
+
+            
         });
 
     });
